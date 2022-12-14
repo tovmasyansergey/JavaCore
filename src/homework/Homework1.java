3e0d6cbd3c63a581b5c2c29a04c5f508b0204985
tovmasyansergey <serg.yan55555@gmail.com>
2022-12-14 23:59:55 +0400

    homework 2

--- src/homework/Homework1.java
+++ src/homework/Homework1.java
@@ -2,6 +2,6 @@ package homework;
 
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("hello from homework");
+        System.out.println("hello from homework 2");
     }
 }
