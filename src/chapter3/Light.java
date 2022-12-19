59606cbc4b64e4c5939b5fb16a7f38840f19645b
tovmasyansergey <serg.yan55555@gmail.com>
2022-12-19 18:33:56 +0400

    homework1FigurePainter

--- src/chapter3/Light.java
+++ src/chapter3/Light.java
@@ -22,4 +22,20 @@ public class Light {
         distance = lightspeed * seconds ;
         System.out.println("za " + days + " dney svet proydet okolo " + distance + " mil. ");
     }
+
+    public static class IfForExamplehome {
+        public static void main(String[] args) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 5; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int b = 0; b < i; b++) {
+                    System.out.print("*");
+                }
+
+
+                System.out.println();
+            }
+        }
+    }
 }
---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,40 @@
+package homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        //figure 1
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        //figure 2
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = -1; k < i; k++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        //figure 3
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+
+}
