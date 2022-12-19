19b03343b20d130642095bf4b728771277ae55cd
tovmasyansergey <serg.yan55555@gmail.com>
2022-12-19 17:12:30 +0400

    chapter3AutoArray

---
+++ src/chapter3/autoArray.java
@@ -0,0 +1,8 @@
+package chapter3;
+
+public class autoArray {
+    public static void main(String[] args) {
+        int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("v aprele" + month_days[3] + "days");
+    }
+}
