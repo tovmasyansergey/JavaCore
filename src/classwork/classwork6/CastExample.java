32448578e5cfb4bc83e9685cc6830245c10b1a74
tovmasyansergey <serg.yan55555@gmail.com>
2023-10-13 19:57:15 +0400

    Cast example

--- src/classwork/classwork6/CastExample.java
+++ src/classwork/classwork6/CastExample.java
@@ -1,10 +1,14 @@
 package classwork.classwork6;
 
-public class CharExample {
+public class CastExample {
     public static void main(String[] args) {
         int b = 1000;
         byte c;
         c = (byte) b;
         System.out.println(c);
+        char a  = 890;
+        byte l;
+        l = (byte) a;
+        System.out.println(l);
     }
 }
