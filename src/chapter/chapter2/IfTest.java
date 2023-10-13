5b6492509e2ac3828b357615b8ae0d15b39ca7da
tovmasyansergey <serg.yan55555@gmail.com>
2023-10-13 20:19:09 +0400

    change Iftest

--- src/chapter/chapter2/IfTest.java
+++ src/chapter/chapter2/IfTest.java
@@ -2,44 +2,23 @@ package chapter.chapter2;
 
 public class IfTest {
     public static void main(String[] args) {
-
-       /* int x, y;
-        x = 10 ;
-        y = 20 ;
-        if( x < y){
-            System.out.println("x menshe y");
-        }
-        x = x * 2;
-        if( x == y){
-            System.out.println("x teper ravno y");
-
-        }
-        x = x * 2;
-        if ( x > y){
-            System.out.println("x teper bolshe y");
-
-        }
-
-
-    }
-}
-}
-        */
         int x, y;
         x = 10;
         y = 20;
-//        if( x < y) {
-//            System.out.println("x menshe y");
-//        }
-//        x = x * 2;
-//        if (x == y){
-//            System.out.println("x ravno y");
-//
-//        }
-        x = x * 8;
-        if (x > y) {
-            System.out.println("x bolshe y");
-        }
+        if (x < y) {
+            System.out.println("x < y");
+            x = x * 2;
+            if (x == y) {
+                System.out.println("x = y");
+            }
+            x = x * 2;
+            if (x > y) {
+                System.out.println("x > y");
 
+            }
+            if (x == y){
+                System.out.println("x = y");//this kod dont work
+            }
+        }
     }
 }
