d9a7ba8cc4c3646c6a7e6e9c6af7b4081be8eb57
tovmasyansergey <serg.yan55555@gmail.com>
2023-10-13 19:44:52 +0400

    1

--- src/classwork/classwork5/Example2.java
+++ src/classwork/classwork5/Example2.java
@@ -2,7 +2,12 @@ package classwork.classwork5;
 
 public class Example2 {
     public static void main(String[] args) {
-       char v = 'v';
+       char v = 101;
         System.out.println(v);
+        int n = 10;
+        for (int i = 0; i < 10; i++) {
+            n += i;
+        }
+        System.out.println(n);
     }
 }
---
+++ src/classwork/classwork6/CharExample.java
@@ -0,0 +1,10 @@
+package classwork.classwork6;
+
+public class CharExample {
+    public static void main(String[] args) {
+        int b = 1000;
+        byte c;
+        c = (byte) b;
+        System.out.println(c);
+    }
+}
